baff0ce8bcb8717907d313eca3531cd35ba5d336
ayumi5 <ayumi.highroof@gmail.com>
2015-09-22 10:14:28 +1200

    add xhr

--- actions/userSession.js
+++ actions/userSession.js
@@ -1,3 +1,5 @@
+import xhr from 'xhr';
+
 export const PROCESS_USER = "PROCESS_USER";
 export const USER_LOGIN = "USER_LOGIN";
 export const LOGIN_FAILED = "LOGIN_FAILED";
@@ -22,6 +24,24 @@ function processUser(user){
 export function authenticate(user) {
   return dispatch => {
     dispatch(processUser(user))
+    return xhr({
+      json: {user},
+      uri: 'http://localhost:3000/authenticate',
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }, function (err, resp, body) {
+      if(resp.statusCode >= 300) {
+        dispatch(loginFailed(user))
+      } else {
+        if(resp.body.auth_token) {
+          user['auth_token'] = resp.body.auth_token
+          dispatch(userLogin(user))
+        } else {
+          dispatch(loginFailed(user))
+        }
+      }
+    })
   }
-  
 }
